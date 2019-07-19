91cbcdb60fcd5789825e1dfd2f6dfbe948eece20
Sergey <dropmailqwade@gmail.com>
2019-07-19 10:26:55 +0300

    v1

---
+++ index.js
@@ -0,0 +1,60 @@
+const http = require('http');
+const fs = require('fs');
+// const path = require('path');
+var htmlFile;
+var mainCss;
+
+const server = http.createServer((req, res)=>{
+
+    fs.readFile('./public/index.html', function(err, pizdec){
+        if(err) throw err;
+        htmlFile = pizdec;
+    });
+    fs.readFile('./public/main.css', function(err, neponyatno){
+        mainCss = neponyatno;
+    });
+    switch(req.url){
+        case 'main.css':
+                res.writeHead(200, {"Content-Type":"text/css"});
+                res.write(mainCss);
+                break;
+        default:
+            res.write(htmlFile);
+            res.writeHead(200,{"Content-Type":"text/html"});
+            
+    }
+    res.end();  
+// bilo na uroke    
+    
+    //     res.writeHead(200, {"Content-Type":"text/html"})
+    //     fs.readFile(__dirname + '/public/index.html' + req.url, 'utf-8', (err, data)=>{
+    //         res.end(data);
+
+    //     })
+    // } else{
+    //     res.writeHead(200, {"Content-Type":"text/html"})
+    //     fs.readFile(__dirname + '/public/' + req.url, 'utf-8', (err, data)=>{
+    //         res.end(data);
+
+    //     })
+    // }
+        
+
+    
+    // if(req.url ==="/"){  
+    //     fs.readFile(__dirname + '/public/index.html', 'utf-8',(err, data)=>{
+    //     res.end(data);   
+    // });
+    // }else if( req.url ==='/pig'){
+    //     res.writeHead(200, {"Content-Type":"text/html"})
+    //     res.end('<h1>PIG dance</h2> <img src="https://media1.tenor.com/images/d42d8a76cb7df3e21bae926a48e20dec/tenor.gif?itemid=5221459">');
+    // } else {
+    //     res.writeHead(200, {"Content-Type":"text/html"})
+    //     res.end('<h1>uhadi 404</h1><img src="https://cs6.pikabu.ru/images/big_size_comm/2014-12_5/14195184557828.jpg">');
+    // }
+})  
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, ()=>{
+    console.log('Server succesfully started, listening on 3000 port');  
+})
